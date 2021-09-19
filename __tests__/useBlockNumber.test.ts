5df82a69b67a04424f3c1e6f6fc126681b2ca6a3
Shravan Sunder <shravan.retail@gmail.com>
2021-09-19 18:31:28 -0400

    #25 🧪 setup jest and created a stub test.  Changed useOnRepetition deps

--- __tests__/useBlockNumber.test.ts
+++ __tests__/useBlockNumber.test.ts
@@ -1,3 +1,13 @@
+import { getMockProvider } from '~helpers/getMockProvider';
+import { renderTestHook } from '~helpers/renderTestHook';
+import { useBlockNumber } from '~~/useBlockNumber';
+
 describe('useBlockNumber', () => {
-  it('When the provider receives a new block, then the block returns the block number', async () => {});
+  const mockProvider = getMockProvider();
+  it('When the provider receives a new block, then the block returns the block number', () => {
+    const test = renderTestHook(() => useBlockNumber(mockProvider));
+
+    console.log(test.result.current);
+    // waitFor(()=> expect)
+  });
 });
--- helpers/renderTestHook.tsx
+++ helpers/renderTestHook.tsx
@@ -4,7 +4,7 @@ import { MockEthersWrapper } from '~helpers/MockWeb3Provider';
 
 type TRenderHook = typeof renderHook;
 
-export const renderEthersHook: TRenderHook = (callback, options = {}) => {
+export const renderTestHook: TRenderHook = (callback, options = {}) => {
   const Wrapper = <MockEthersWrapper />;
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
