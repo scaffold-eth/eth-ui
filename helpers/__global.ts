a77a3f3af9fc59871ddcd982ee0ad5cfb6b3f66e
Shravan Sunder <shravan.retail@gmail.com>
2021-09-20 23:53:20 -0400

    feat: #29 meged lock, updated packages

--- helpers/__global.ts
+++ helpers/__global.ts
@@ -1,22 +1,22 @@
-/* eslint-disable unused-imports/no-unused-vars-ts */
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import 'setimmediate';
-import nodeBuffer from 'buffer';
-import nodeProcess from 'process';
+// /* eslint-disable unused-imports/no-unused-vars-ts */
+// /* eslint-disable @typescript-eslint/no-unsafe-member-access */
+// import 'setimmediate';
+// import nodeBuffer from 'buffer';
+// import nodeProcess from 'process';
 
-import { ExternalProvider } from '@ethersproject/providers/src.ts/web3-provider';
+// import { ExternalProvider } from '@ethersproject/providers/src.ts/web3-provider';
 
-(window as any).global = window;
-const global = window;
+// (window as any).global = window;
+// const global = window;
 
-if (!global.hasOwnProperty('Buffer')) {
-  (global as any).Buffer = nodeBuffer.Buffer;
-}
+// if (!global.hasOwnProperty('Buffer')) {
+//   (global as any).Buffer = nodeBuffer.Buffer;
+// }
 
-(global as any).process = nodeProcess;
+// (global as any).process = nodeProcess;
 
-interface Window {
-  ethereum: ExternalProvider;
-  location: any;
-  localStorage: { getItem: (key: string) => any; setItem: (key: string, value: string) => any };
-}
+// interface Window {
+//   ethereum: ExternalProvider;
+//   location: any;
+//   localStorage: { getItem: (key: string) => any; setItem: (key: string, value: string) => any };
+// }
