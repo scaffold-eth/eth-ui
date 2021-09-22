1ad031fba86ab3e23ca6b9b6ecf8887efddfd29d
Shravan Sunder <shravan.retail@gmail.com>
2021-09-22 00:01:28 -0400

    #33 updated reporter again

--- .mocharc.js
+++ .mocharc.js
@@ -2,5 +2,8 @@ module.exports = {
   spec: 'test/**/*.test.{ts,tsx}',
   require: 'ts-node/register',
   timeout: 10000,
-  reporter: process.env.GITHUB_ACTIONS === 'true' ? 'mocha-github-actions-reporter' : 'spec',
+  reporter: process.env.GITHUB_ACTIONS === 'true' ? 'mocha-junit-reporter' : 'spec',
+  reporterOptions: {
+    mochaFile: './test/testresult.xml',
+  },
 };
