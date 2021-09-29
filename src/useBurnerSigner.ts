ec94fe5743aac87bfcafe828419346fff1f3cedd
Shravan Sunder <shravan.retail@gmail.com>
2021-09-28 23:13:49 -0400

    feat: useBurner encompasses the creation, use, saving of burner wallet

--- src/useBurnerSigner.ts
+++ src/useBurnerSigner.ts
@@ -1,18 +1,45 @@
-import { SigningKey } from '@ethersproject/signing-key';
 import { BytesLike, ethers, Signer } from 'ethers';
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useCallback } from 'react';
 
 import { TEthersProvider } from '~~/models/providerTypes';
+
+const isValidPk = (pk: BytesLike | undefined | null): boolean => {
+  return pk?.length === 64 || pk?.length === 66;
+};
+
+export const saveBurnerKeyToStorage = (incomingPK: BytesLike): void => {
+  if (isValidPk(incomingPK)) {
+    const rawPK = incomingPK;
+    window.history.pushState({}, '', '/');
+    const currentPrivateKey = window.localStorage.getItem('metaPrivateKey');
+    if (currentPrivateKey && currentPrivateKey !== rawPK) {
+      window.localStorage.setItem(`metaPrivateKey_backup${Date.now()}`, currentPrivateKey);
+      console.log('🔑 ...Saved Private Key');
+    }
+    window.localStorage.setItem('metaPrivateKey', rawPK.toString());
+  }
+};
+
+export const loadBurnerKeyFromStorage = (): string | null => {
+  const currentPrivateKey = window.localStorage.getItem('metaPrivateKey');
+  return currentPrivateKey;
+};
+
+export interface IBurnerSignerManager {
+  signer: Signer | undefined;
+  saveToStorage: () => void;
+  loadFromStorage: () => void;
+}
 
 /**
  * A hook that creates a buner address and returns a Signer
  * @param provider (TEthersProvider)
  * @returns (ethers.signer) :: signer of the wallet
  */
-export const useBurnerSigner = (provider: TEthersProvider): Signer | undefined => {
+export const useBurnerSigner = (provider: TEthersProvider): IBurnerSignerManager => {
   const key = 'metaPrivateKey';
   const [signer, setSigner] = useState<Signer>();
-  const [privateKeyValue, setPrivateKeyValue] = useState<BytesLike | SigningKey>();
+  const [privateKeyValue, setPrivateKeyValue] = useState<BytesLike>();
 
   const setValue = (value: any): void => {
     try {
@@ -43,5 +70,23 @@ export const useBurnerSigner = (provider: TEthersProvider): Signer | undefined =
     }
   }, [privateKeyValue, provider]);
 
-  return signer;
+  /**
+   * if valid save burner key to storage
+   */
+  const saveToStorage = useCallback(() => {
+    console.log('🔑 Incoming Private Key...');
+    if (privateKeyValue != null) {
+      saveBurnerKeyToStorage(privateKeyValue);
+    }
+  }, [privateKeyValue]);
+
+  const loadFromStorage = useCallback(() => {
+    const pk = loadBurnerKeyFromStorage();
+    if (pk && isValidPk(pk)) {
+      console.log('🔑 ...Loaded Private Key');
+      setPrivateKeyValue(pk);
+    }
+  }, []);
+
+  return { signer, saveToStorage, loadFromStorage };
 };
--- src/useUserProviderAndSigner.ts
+++ src/useUserProviderAndSigner.ts
@@ -5,23 +5,6 @@ import { useMemo, useState } from 'react';
 import { parseProviderOrSigner } from '~~/functions/parseProviderOrSigner';
 import { TProviderAndSigner, TEthersProvider } from '~~/models';
 
-const syncBurnerKeyFromStorage = (): void => {
-  if (window.location.pathname && window.location.pathname.includes('/pk')) {
-    const incomingPK = window.location.hash.replace('#', '');
-
-    if (incomingPK.length === 64 || incomingPK.length === 66) {
-      console.log('🔑 Incoming Private Key...');
-      const rawPK = incomingPK;
-      window.history.pushState({}, '', '/');
-      const currentPrivateKey = window.localStorage.getItem('metaPrivateKey');
-      if (currentPrivateKey && currentPrivateKey !== rawPK) {
-        window.localStorage.setItem(`metaPrivateKey_backup${Date.now()}`, currentPrivateKey);
-      }
-      window.localStorage.setItem('metaPrivateKey', rawPK);
-    }
-  }
-};
-
 /**
  *  Gets user provider/signer from injected provider or local provider
  *  Use your injected provider from 🦊 Metamask 
