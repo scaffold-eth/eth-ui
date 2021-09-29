891b167c787894c361bfd9d802c5b7732fbe5444
Shravan Sunder <shravan.retail@gmail.com>
2021-09-29 19:30:50 -0400

    feat: #37 ✨ useWeb3Modal  updates

--- src/useWeb3Modal.ts
+++ src/useWeb3Modal.ts
@@ -1,15 +1,16 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { Web3Provider } from '@ethersproject/providers';
 import { useCallback, useEffect, useRef } from 'react';
-import Web3Modal, { ICoreOptions } from 'web3modal';
+import Web3Modal, { ICoreOptions, ThemeColors } from 'web3modal';
 import './helpers/_global';
 
 import { TEthersProvider } from '~~/models';
 
 export interface IWeb3ModalState {
   initialized: boolean;
-  openWeb3Modal: () => void;
-  logoutOfWeb3Modal: () => void;
+  openWeb3ModalCallback: () => void;
+  logoutOfWeb3ModalCallback: () => void;
+  updateWeb3ModalThemeCallback: (theme: ThemeColors | string) => void;
 }
 
 const const_web3DialogClosedByUser = 'Modal closed by user';
@@ -20,17 +21,22 @@ export const useWeb3Modal = (
   const web3ModalProviderRef = useRef<Web3Modal>();
   const initalizedRef = useRef<boolean>();
 
+  /**
+   * initalize web3 object and save it to state
+   */
   useEffect(() => {
     if (!Web3Modal) {
       throw 'Web3Modal is a peer dependancy to use this hook';
     }
-
     web3ModalProviderRef.current = new Web3Modal(web3ModalConfig ?? {});
     initalizedRef.current = false;
     setCurrentProvider(undefined);
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [web3ModalConfig]);
 
+  /**
+   * a callback to reload the page and clear cache
+   */
   const reload = useCallback(() => {
     return (_param: any): void => {
       web3ModalProviderRef.current?.cachedProvider &&
@@ -40,7 +46,10 @@ export const useWeb3Modal = (
     };
   }, []);
 
-  const logoutOfWeb3Modal = useCallback(
+  /**
+   * log out of web3modal provider
+   */
+  const logoutOfWeb3ModalCallback = useCallback(
     (reload: boolean = false): void => {
       web3ModalProviderRef.current?.clearCachedProvider();
       setCurrentProvider(undefined);
@@ -51,9 +60,12 @@ export const useWeb3Modal = (
       }
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    [web3ModalProviderRef]
+    [setCurrentProvider]
   );
 
+  /**
+   * the function that setups up and loads the web3modal
+   */
   const loadWeb3Modal = useCallback(async () => {
     try {
       const provider = await web3ModalProviderRef.current?.connect();
@@ -74,7 +86,7 @@ export const useWeb3Modal = (
         // Subscribe to session disconnection
         provider.on('disconnect', (code: any, reason: any) => {
           console.log(code, reason);
-          void logoutOfWeb3Modal();
+          void logoutOfWeb3ModalCallback();
         });
       }
     } catch (e) {
@@ -83,21 +95,34 @@ export const useWeb3Modal = (
       }
       console.log(e);
     }
-  }, [setCurrentProvider, logoutOfWeb3Modal]);
+  }, [setCurrentProvider]);
 
+  /**
+   * On initalization, load modal
+   */
   useEffect(() => {
-    // if (web3ModalProviderRef.current.cachedProvider) {
-    void loadWeb3Modal();
-    // }
+    if (web3ModalProviderRef.current?.cachedProvider) {
+      void loadWeb3Modal();
+    }
   }, [loadWeb3Modal]);
 
-  const openWeb3Modal = useCallback(() => {
+  /**
+   * a callback to open
+   */
+  const openWeb3ModalCallback = useCallback(() => {
     setCurrentProvider(undefined);
     web3ModalProviderRef.current?.clearCachedProvider();
     void loadWeb3Modal();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [loadWeb3Modal]);
 
+  const updateWeb3ModalThemeCallback = useCallback((theme: ThemeColors | string) => {
+    web3ModalProviderRef.current?.updateTheme(theme);
+  }, []);
+
+  /**
+   * add hooks to reload page if required
+   */
   useEffect(() => {
     /* eslint-disable */
     if (window?.ethereum?.on && window?.ethereum?.off) {
@@ -111,5 +136,10 @@ export const useWeb3Modal = (
     /* eslint-disable */
   }, [window?.ethereum]);
 
-  return { initialized: initalizedRef.current ?? false, openWeb3Modal, logoutOfWeb3Modal };
+  return {
+    initialized: initalizedRef.current ?? false,
+    openWeb3ModalCallback,
+    logoutOfWeb3ModalCallback,
+    updateWeb3ModalThemeCallback,
+  };
 };
