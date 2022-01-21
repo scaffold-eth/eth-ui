2ba85bf630d1c2c91bfa7c5c218659972eba4fa8
Shravan Sunder <ShravanSunder@users.noreply.github.com>
2022-01-21 09:48:27 -0500

    #122 fixed final check

--- src/helpers/test-utils/constants/testConstants.ts
+++ src/helpers/test-utils/constants/testConstants.ts
@@ -2,6 +2,10 @@ export const const_basicGasPrice = 875000000;
 
 export const const_DefaultTestChainId = 31337;
 
+/**
+ *
+ * This is a const based on WaitOptions from react testing lib
+ */
 export const defaultBlockWaitOptions: { timeout: number; interval: number } = {
   timeout: 10_000,
   interval: 200,
