2ea4a552994ba8d1879cbdb45d8987a611f2ec2d
tobias <61471326+TobiasBK@users.noreply.github.com>
2022-03-01 09:01:11 -0500

    homepage design update (#179)
    * homepage design update
    
    * PR update,added FC
    
    Co-authored-by: Shravan Sunder <ShravanSunder@users.noreply.github.com>

--- docusaurus/src/components/HomepageFeatures.tsx
+++ docusaurus/src/components/HomepageFeatures.tsx
@@ -1,48 +1,51 @@
 import useBaseUrl from '@docusaurus/useBaseUrl';
-import React from 'react';
+import React, { FC } from 'react';
 import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
-type FeatureItem = {
+interface FeatureItem {
   title: string;
   image: string;
-  description: JSX.Element;
+  description: object;
 };
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Fast & Easy React Hooks',
     image: '/img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and used to get your website up and running
-        quickly.
+        Speed up your frontend development with pre-built hooks for use with context,
+        allowing easy use throughout your application.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Optimized Web3 Development',
     image: '/img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go ahead and move your docs into the{' '}
-        <code>docs</code> directory.
+        A dedicated cache to reduce RPC calls. Several different network update options.
+        Typed contracts to increase clarity.
+        These are just a few of the benefits you get with eth-hooks.
+
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Seemless Smart Contract Interaction',
     image: '/img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can be extended while reusing the same
-        header and footer.
+        We use the <a href="https://docs.ethers.io/v5/" target="_blank">ethers.js</a> library under-the-hood.
+        This gives you reliable read and write controls to smart contracts and
+        even lets you pass different ethers providers.
       </>
     ),
   },
 ];
 
-function Feature({ title, image, description }: FeatureItem) {
+const Feature = ({ title, image, description }: FeatureItem) => {
   return (
     <div className={clsx('col col--4')}>
       <div className="text--center">
@@ -56,14 +59,14 @@ function Feature({ title, image, description }: FeatureItem) {
   );
 }
 
-export default function HomepageFeatures(): JSX.Element {
+export const HomepageFeatures: FC = () => {
   return (
     <section className={styles.features}>
       <div className="container">
         <div className="row">
           {FeatureList.map((props, idx) => (
             <Feature key={idx} {...props} />
-          ))}
+          ))} 
         </div>
       </div>
     </section>
